fd8500f4718884981eb591102540b5356d82be2f
medamine <mohamedamineammach@gmail.com>
2025-03-16 23:47:08 +0000

    change basic infos to worktreewise

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,20 @@
 import {Footer, Layout, Navbar} from 'nextra-theme-docs'
-import {Banner, Head} from 'nextra/components'
+import {Head} from 'nextra/components'
 import {getPageMap} from 'nextra/page-map'
 import 'nextra-theme-docs/style.css'
-import Link from "next/link";
 
 export const metadata = {
     // Define your metadata here
     // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
 }
 
-const banner = <Banner storageKey="some-key">This template was created with 🩸 and 💦 by <Link href="https://github.com/phucbm">PHUCBM</Link> 🐧</Banner>
 const navbar = (
     <Navbar
-        logo={<img src="/images/general/logo.svg" alt="Logo" width={100} height={20}/>}
+        logo={<img src="/images/general/logo.svg" alt="Logo" width={240} height={100}/>}
         // ... Your additional navbar options
     />
 )
-const footer = <Footer>MIT {new Date().getFullYear()} © Nextra.</Footer>
+const footer = <Footer>{new Date().getFullYear()} © WorktreeWise.</Footer>
 
 export default async function RootLayout({children}) {
     return (
@@ -36,7 +34,6 @@ export default async function RootLayout({children}) {
         </Head>
         <body>
         <Layout
-            banner={banner}
             navbar={navbar}
             pageMap={await getPageMap()}
             docsRepositoryBase="https://github.com/phucbm/nextra-docs-starter/tree/main"
