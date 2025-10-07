ed594d38b1753e197c30d2d7dcaa2e0e2d475134
medamine <mohamedamineammach@gmail.com>
2025-10-07 15:58:59 +0100

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,6 +46,18 @@ export default async function RootLayout({children}) {
               a.appendChild(r);
           })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
             </Script>
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=G-Y1EESLCDEJ"
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-Y1EESLCDEJ');
+          `}
+            </Script>
         </Head>
         <body>
         <Layout
